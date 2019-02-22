ea6a8b4f5ce345d6ed391dfe36bd79cd2e74426b
npocmu <npocmu@gmail.com>
2019-02-22 17:33:31 +0300

    try to resolve routes

--- TBeyond4_PtP/Source/Common/Objects/Persistent/MerchantsUnderwayInfo.js
+++ TBeyond4_PtP/Source/Common/Objects/Persistent/MerchantsUnderwayInfo.js
@@ -8,7 +8,7 @@ MerchantUnderwayInfo
    s_id:      number    - id of map source cell
    d_id:      number    - id of map destination cell
 
-   xn:        number    - number of sheduled routes
+   xn:        number    - number of scheduled routes
 
    own_id:    number    - user ID for merchant owner
    rec_id:    number    - user ID for merchant recepient
@@ -57,9 +57,51 @@ function MerchantsUnderwayInfo()
 }
 
 /////////////////////////////////////////////////////////////////////
+// Convert scheduled trade routes to events
+function _getTradeRoutesEventsQueue(villageId, resourcesEventsQueue)
+{
+   var srcVillageId;
+   var marketRoutesInfo;
+   var ttServer = toTimeStamp(TB3O.serverTime);
+   var dtMidnight = new Date(TB3O.serverTime.getYear(),TB3O.serverTime.getMonth(),TB3O.serverTime.getDay());
+   var ttMidnight = toTimeStamp(dtMidnight);
+
+   //var destXY = id2xy(merchantUnderwayInfo.d_id);
+
+   for ( srcVillageId in TB3O.VillagesInfo )
+   {
+      if ( srcVillageId != villageId )
+      {
+         marketRoutesInfo = TB3O.VillagesMRInfo.load(srcVillageId);
+         if ( marketRoutesInfo.routes && marketRoutesInfo.ttUpd !== undefined )
+         {
+            for ( var i = 0; i < marketRoutesInfo.routes.length; ++i )
+            {
+               var marketRouteInfo = marketRoutesInfo.routes[i];
+               if ( marketRouteInfo.d_vid == villageId && marketRouteInfo.enabled )
+               {
+                  var ttStart = ttMidnight + marketRouteInfo.tsStart*1000;
+                  if ( ttStart > ttServer )
+                  {
+                      
+                     __DUMP__(marketRouteInfo)
+                     /*
+                     var qDist = getDistance(destXY[0], destXY[1], srcXY[0], srcXY[1]);
+                     var ttEnd = ttStart + getMerchantTime(xDist, race) * 1000;
+                     var resourcesEvent = new ResourcesEvent(marketRouteInfo.Res, bIncoming, false, ttEnd, marketRouteInfo);
+                     resourcesEventsQueue.push(resourcesEvent);
+                     *
+                  }
+               }
+            }
+         }
+      }
+   }
+}
+
+/////////////////////////////////////////////////////////////////////
 // Convert merchantsUnderwayInfo to ResourcesEventsQueue
 // Return: ResourcesEventsQueue (UNSORTED!)
-//function getMerchantsUnderwayResourcesEventsQueue(villageIdmerchantsUnderwayInfo)
 function getVillageResourcesEventsQueue(villageId)
 {
    var resourcesEventsQueue = [];
@@ -92,38 +134,42 @@ function getVillageResourcesEventsQueue(villageId)
 
    var k, merchantUnderwayInfo, resourcesEvent;
 
-   // copy incoming merchants to events and resolve sheduled routes only for own merchants
-   // (its hard to detect race for other merchants and accurate calculate timestamps for events)
-   for ( k = 0; k < merchantsUnderwayInfo.i.length; ++k )
+   if ( merchantsUnderwayInfo.ttUpd !== undefined )
    {
-      merchantUnderwayInfo = merchantsUnderwayInfo.i[k];
-
-      if ( merchantUnderwayInfo.ttArrival > ttServer )
+      // copy incoming merchants to events and resolve scheduled routes only for own merchants
+      // (its hard to detect race for other merchants and accurate calculate timestamps for events)
+      for ( k = 0; k < merchantsUnderwayInfo.i.length; ++k )
       {
-         resourcesEvent = new ResourcesEvent(merchantUnderwayInfo.Res, true, true, merchantUnderwayInfo.ttArrival, merchantUnderwayInfo);
-         resourcesEventsQueue.push(resourcesEvent);
+         merchantUnderwayInfo = merchantsUnderwayInfo.i[k];
+
+         if ( merchantUnderwayInfo.ttArrival > ttServer )
+         {
+            resourcesEvent = new ResourcesEvent(merchantUnderwayInfo.Res, true, true, merchantUnderwayInfo.ttArrival, merchantUnderwayInfo);
+            resourcesEventsQueue.push(resourcesEvent);
+         }
+
+         if ( merchantUnderwayInfo.own_id == TB3O.UserID )
+         {
+            resolveRoutes(merchantUnderwayInfo, true, 0);
+         }
       }
 
-      if ( merchantUnderwayInfo.own_id == TB3O.UserID )
+      // copy outgoing merchants to events only if they has scheduled routes
+      for ( k = 0; k < merchantsUnderwayInfo.o.length; ++k )
       {
-         resolveRoutes(merchantUnderwayInfo, true, 0);
+         merchantUnderwayInfo = merchantsUnderwayInfo.o[k];
+         resolveRoutes(merchantUnderwayInfo, false, 1);
+      }
+
+      // copy returning merchants to events if they has scheduled routes
+      for ( k = 0; k < merchantsUnderwayInfo.r.length; ++k )
+      {
+         merchantUnderwayInfo = merchantsUnderwayInfo.r[k];
+         resolveRoutes(merchantUnderwayInfo, false, 2);
       }
    }
 
-   // copy outgoing merchants to events only if they has sheduled routes
-   for ( k = 0; k < merchantsUnderwayInfo.o.length; ++k )
-   {
-      merchantUnderwayInfo = merchantsUnderwayInfo.o[k];
-      resolveRoutes(merchantUnderwayInfo, false, 1);
-   }
-
-
-   // copy returning merchants to events if they has sheduled routes
-   for ( k = 0; k < merchantsUnderwayInfo.r.length; ++k )
-   {
-      merchantUnderwayInfo = merchantsUnderwayInfo.r[k];
-      resolveRoutes(merchantUnderwayInfo, false, 2);
-   }
+   _getTradeRoutesEventsQueue(villageId, resourcesEventsQueue);
 
    return resourcesEventsQueue;
 }
